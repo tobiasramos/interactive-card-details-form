a0b554637ccdaa534f8d85563e12c1b721598206
Tobias <tobiasramossilva@gmail.com>
2022-12-07 19:00:05 -0300

    Removendo o imask para  adicionar o cleave. E validando todos os dados do formulário

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,39 @@
-import IMask from 'imask';
+new Cleave('#nomeTitular', {
+    uppercase: true,
+    blocks: [10, 10]
+})
 
+new Cleave('#numCartao', {
+    blocks: [4, 4, 4, 4],
+    delimiter: ' ',
+    numericOnly: true
+});
+
+new Cleave('#mes', {
+    date: true,
+    datePattern: ['m']
+});
+
+new Cleave('#ano', {
+    date: true,
+    datePattern: ['y']
+});
+
+new Cleave('#codigo', {
+    blocks: [3],
+    numericOnly: true
+});
+
+
+
+
+
+
+
+
+
+
+/*
 let form = document.getElementById('form')
 let nameTitular = document.getElementById('name')
 let numberCard = document.getElementById('numero')
@@ -59,58 +93,10 @@ function sucesso(input) {
 }
 
 
-function novaPage() {
-
-    let resultado = document.getElementById("card")
-
-    resultado.innerHTML = `<svg class="logo-card" width="84" height="47" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <ellipse cx="23.478" cy="23.5" rx="23.478" ry="23.5" fill="#fff" />
-                                <path d="M83.5 23.5c0 5.565-4.507 10.075-10.065 10.075-5.559 0-10.065-4.51-10.065-10.075 0-5.565 
-                                4.506-10.075 10.065-10.075 5.558 0 10.065 4.51 10.065 10.075Z" stroke="#fff" />
-                           </svg>
-                        
-                           <div class="card-front">
-                                <div class="campo-de-numeros">
-                                    <span>${numberCard.value}</span>
-                                </div>
-                                <div class="campo-do-nome">
-                                    <span>${nameTitular.value}</span>
-                                </div>
-                                <div class="campo-da-data">
-                                    <span>${mes.value}</span>/
-                                    <span>${ano.value}</span>
-                                </div>
-                            </div>
-
-                            <div class="card-back">
-                            <div class="campo-do-cvc">
-                                    <span>${codigo.value}</span>
-                                </div>
-                            </div>
-
-                            <div class="registered-data sucesso">
-                            <div>
-                            <svg width="80" height="80" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <circle cx="40" cy="40" r="40" fill="url(#a)" />
-                                <path d="M28 39.92 36.08 48l16-16" stroke="#fff" stroke-width="3" />
-                                <defs>
-                                    <linearGradient id="a" x1="-23.014" y1="11.507" x2="0" y2="91.507" gradientUnits="userSpaceOnUse">
-                                        <stop stop-color="#6348FE" />
-                                        <stop offset="1" stop-color="#610595" />
-                                    </linearGradient>
-                                </defs>
-                            </svg>
-                            <h2>Obrigado</h2>
-                            <p>Adicionamos os detalhes do seu cartão</p>
-                        </div></div>
-                            `
-}
-
-
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     checkInputs();
     novaPage();
 
-})
+})*/
